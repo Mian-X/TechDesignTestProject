b10331180552dab5a0b4d9ba37268edbca99dd04
MianX <84510202+Mian-X@users.noreply.github.com>
2025-03-21 10:44:09 +0500

    Add sounds to main menu
    Added background music for main menu, as well as the sfx of button clicks and animation of the start/loading of the scene.

--- TechDesignTestProject/Assets/Game/_Scripts/GameManager.cs
+++ TechDesignTestProject/Assets/Game/_Scripts/GameManager.cs
@@ -1,12 +1,60 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Tooltip("Delay (in seconds) before loading the scene")]
+    public float delay;
 
-    // Load Scene by index
-    public void LoadScene(int index)
+    [Tooltip("Set an animation to move from the current scene to the next one")]
+    public AnimationClip loadNextSceneClip;
+
+    [Tooltip("Set an animator with your animation clip")]
+    public Animator animator;
+
+    private void Start()
     {
+        if (loadNextSceneClip == null) Debug.LogError("Missing {loadNextSceneClip}!");
+        if (animator == null) Debug.LogError("Missing animator!");
+    }
+
+    // Return true if animation clip in animator states found, return false if is not
+    private bool HasAnimationClip(string name)
+    {
+        var controller = animator.runtimeAnimatorController as UnityEditor.Animations.AnimatorController;
+
+        if (controller == null)
+        {
+            Debug.Log("AnimatorController not found");
+            return false;
+        }
+
+        foreach (var layer in controller.layers)
+        {
+            var stateMachine = layer.stateMachine;
+            foreach (var state in stateMachine.states)
+            {
+                if (state.state.name == name) return true;
+            }
+        }
+        return false;
+    }
+
+    public void LoadSceneWithDelay(int index)
+    {
+        StartCoroutine(LoadScene(index));
+    }
+
+    // Load Scene by index with delay
+    private IEnumerator LoadScene(int index)
+    {
+        if (HasAnimationClip(loadNextSceneClip.name) == true) animator.Play(loadNextSceneClip.name);
+
+        // Wait {delay} seconds before loading scene
+        yield return new WaitForSeconds(delay);
+
+        // Load scene
         SceneManager.LoadScene(index);
     }
 
